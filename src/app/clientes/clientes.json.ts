c670e22f8a59df034408e1df8195709baea91c8a
Cristian David Pinzón <pinzon.malaverc@gmail.com>
2022-10-28 15:04:46 -0500

    Seccion 2 y 3 sobre Angular

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from './cliente'
+
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Cris', apellido:'Pinzon',email:'crisp@gmal.com', createAt: '2017-12-11'},
+  {id: 2, nombre: 'Rasmus', apellido:'Guzman',email:'Rasmus1@gmal.com', createAt: '2017-12-23'},
+  {id: 3, nombre: 'Erich', apellido:'Tique',email:'Erich3@gmal.com', createAt: '2017-11-11'},
+  {id: 4, nombre: 'Richard', apellido:'Perez',email:'Richards@gmal.com', createAt: '2017-12-13'},
+  {id: 5, nombre: 'Kuis', apellido:'Gomez',email:'Kuiss@gmal.com', createAt: '2017-12-14'},
+  {id: 6, nombre: 'Ana', apellido:'Munevar',email:'Anaa@gmal.com', createAt: '2017-12-23'},
+  {id: 7, nombre: 'Angie', apellido:'Forero',email:'Angiej@gmal.com', createAt: '2017-12-22'},
+  {id: 8, nombre: 'Paola', apellido:'Garcia',email:'Paolaj@gmal.com', createAt: '2017-12-14'},
+  {id: 9, nombre: 'Laura', apellido:'Pinzon',email:'Laurap@gmal.com', createAt: '2017-10-14'},
+];
